1c613269f1663d59c329b01d2b48d1fe1ca6a0be
petar-git <31828447+petar97@users.noreply.github.com>
2024-10-25 15:30:26 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  reactStrictMode: true,
-  swcMinify: true,
+  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: isProd ? '/portfolio' : '',
+  output: 'export'
 };
 
-module.exports = nextConfig;
+export default nextConfig;
